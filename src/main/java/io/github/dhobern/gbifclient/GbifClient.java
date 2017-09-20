f0199192acc1067b4c96d588815df75871e59bec
dhobern <dhobern@gmail.com>
2017-09-20 08:12:52 +0200

    Configuration

--- src/main/java/io/github/dhobern/gbifclient/GbifClient.java
+++ src/main/java/io/github/dhobern/gbifclient/GbifClient.java
@@ -50,8 +50,8 @@ public class GbifClient {
                     
                     OccurrenceMatrix<GridCell,OccurrenceBin> gridMatrix = gridOccurrenceBins(binMatrix);
 
-                    gridMatrix.exportGrid("GridExport-Ranked-" + scientificName.replace(" ", "_") + ".txt", OccurrenceMatrix.FORMAT_RANKORDER);
-                    gridMatrix.exportGrid("GridExport-Occupancy-" + scientificName.replace(" ", "_") + ".txt", OccurrenceMatrix.FORMAT_OCCUPANCY);
+                    gridMatrix.exportGrid("GridExport-" + scientificName.replace(" ", "_") + "-Ranked.txt", OccurrenceMatrix.FORMAT_RANKORDER);
+                    gridMatrix.exportGrid("GridExport-" + scientificName.replace(" ", "_") + "-Occupancy.txt", OccurrenceMatrix.FORMAT_OCCUPANCY);
                 }
             }
         } catch (IOException ex) {
@@ -118,97 +118,4 @@ public class GbifClient {
         
         return gridMatrix;
     }
-    
-    /*
-    private static void exportBins(OccurrenceBinManager binMatrix) {
-        try {
-            PrintWriter writer = new PrintWriter("BinExport-" + scientificName.replaceAll(" ", "_") + ".txt", "UTF-8");
-            writer.printf("decimallatitude\tdecimallongitude\teventdate\ttaxonkey\tscientifiname\tcount\n");
-
-            Iterator<OccurrenceBin> iterator = binMatrix.getOccurrenceBins();
-            while (iterator.hasNext()) {
-                OccurrenceBin bin = iterator.next();
-                writer.printf("%f\t%f\t%s\t%s\t%s\t%d\n",
-                              bin.getDecimalLatitudeCentroid(),
-                              bin.getDecimalLongitudeCentroid(),
-                              bin.getDateString(),
-                              bin.getSpeciesKey(),
-                              bin.getCanonicalName(),
-                              bin.getCount());
-            }
-            writer.close();
-        } catch (IOException e) {
-            Logger.getLogger(GbifClient.class.getName()).log(Level.SEVERE, null, e);
-        }
-    }
-
-    private static void exportGrid(GridManager gridMatrix, int maxSpecies) {
-        try {
-            PrintWriter writer = new PrintWriter("GridExport-" + scientificName.replaceAll(" ", "_") + ".txt", "UTF-8");
-            writer.printf("decimallatitude\tdecimallongitude\ttimeperiod\ttotalspecies\ttotalbins\ttotaloccurrences");
-            
-            int digits = 0;
-            for (int n = maxSpecies; n > 0; n /= 10) {
-                digits++;
-            }
-            String formatString = "\trank_%0" + digits + "d";
-            for (int i = 0; i < maxSpecies; i++) {
-               writer.printf(formatString, i);
-            }
-            writer.printf("\n");
-            
-            Species[][][][] grid = gridMatrix.getGrid();
-            int xRange = gridMatrix.getXRange();
-            int yRange = gridMatrix.getYRange();
-            int zRange = gridMatrix.getZRange();
-            
-            String[] xLabels = new String[xRange];
-            String[] yLabels = new String[yRange];
-            String[] zLabels = new String[zRange];
-            
-            for (int i = 0; i < xRange; i++) {
-                xLabels[i] = gridMatrix.getXLabel(i);
-            }
-            for (int i = 0; i < yRange; i++) {
-                yLabels[i] = gridMatrix.getYLabel(i);
-            }
-            for (int i = 0; i < zRange; i++) {
-                zLabels[i] = gridMatrix.getZLabel(i);
-            }
-            
-                for (int j = 0; j < yRange; j++) {
-                for (int i = 0; i < xRange; i++) {
-                    for (int k = 0; k < zRange; k++) {
-                        Species[] cell = grid[i][j][k];
-                        if (cell.length > 0) {
-                            int occurrenceCount = 0;
-                            int binCount = 0;
-                            for(int s = 0; s < cell.length; s++) {
-                                occurrenceCount += cell[s].getOccurrenceCount();
-                                binCount += cell[s].getBinCount();
-                            }
-
-                            writer.printf("%s\t%s\t%s\t%d\t%d\t%d",
-                                    yLabels[j], xLabels[i], zLabels[k],
-                                    cell.length, binCount, occurrenceCount);
-
-                            for (int s = 0; s < maxSpecies; s++) {
-                                writer.printf("\t");
-                                if (s < cell.length) {
-                                    writer.printf("%s [%d / %d]", cell[s].getCanonicalName(), cell[s].getBinCount(), cell[s].getOccurrenceCount());
-                                }
-                            }
-                            
-                            writer.printf("\n");
-                        }
-                    }
-                }
-            }
-
-            writer.close();
-        } catch (IOException e) {
-            Logger.getLogger(GbifClient.class.getName()).log(Level.SEVERE, null, e);
-        }
-    
-    }*/
 }
